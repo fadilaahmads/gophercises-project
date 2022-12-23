0204670419331cffd60e630044d40e27783b8151
fadilaahmads <fadilahmad59@gmail.com>
2022-12-23 20:54:56 +0700

    Add timer

--- Exercise1-Quiz_Game/main.go
+++ Exercise1-Quiz_Game/main.go
@@ -6,7 +6,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
+
+func timer(timeout int, ch chan<- bool) {
+	time.AfterFunc(time.Duration(timeout)*time.Second, func() {
+		ch <- true
+	})
+}
+
+func watcher(timeout int, ch <-chan bool) {
+	<-ch
+	fmt.Println("Timeout! no answer after", timeout, " seconds")
+	os.Exit(0)
+}
 
 func main() {
 	// open CSV file
@@ -21,10 +34,18 @@ func main() {
 	csvReader := csv.NewReader(file)
 
 	data, err := csvReader.ReadAll()
+
+	// timer
+	var timeout = 5
+	var ch = make(chan bool)
+
 	correct_answer := 0
 	for _, value := range data {
 		question := value[0]
 		answer := value[1]
+
+		go timer(timeout, ch)
+		go watcher(timeout, ch)
 
 		fmt.Println("Question: ", question)
 		var input string
